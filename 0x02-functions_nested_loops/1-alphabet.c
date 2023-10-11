0d19e6721d752e897a4c0a9699524895cbbd6f35
ednaakpene <edvalerie18@gmail.com>
2023-10-10 19:29:54 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - task function
+ * Return: return 0
+ */
+
+void print_alphabet(void)
+{
+char e;
+for (e = 'a'; e <= 'z'; e++)
+{
+_putchar(e);
+}
+_putchar('\n');
+}
