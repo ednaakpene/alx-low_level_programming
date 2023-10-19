7385b216366bbc05ae3691ff112bd4c581cc040d
root <root@DESKTOP-DCG0S71.localdomain>
2023-10-19 15:10:58 +0000

    made changes

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,16 +10,18 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-int e;
+int e = 0;
 
-for (e = 0; e < n && src[e] != '\0'; e++)
+while (e < n && src[e] != '\0')
 {
+	dest[e] = src[e];
+	e++;
+}
 	while (e < n)
 	{
 	dest[e] = '\0';
 	e++;
 	}
-}
 return (dest);
 
 }
